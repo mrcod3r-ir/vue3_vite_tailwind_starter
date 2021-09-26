25a6c1dca1e1eccecf3aa54a6d2182f19afe4c37
mrCod3r.ir <mrCod3r.ir@gmail.com>
2021-09-26 14:57:51 +0330

    complete

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
     <h1 class="mb-6 text-3xl font-extrabold">Home</h1>
     <p class="mb-6">Name in store is : {{ name }}</p>
-    <input type="text" class="p-2 mr-4 border border-gray-600 rounded" />
+    <input
+        type="text"
+        v-model="newName"
+        class="p-2 mr-4 border border-gray-600 rounded"
+    />
     <button class="p-2 text-white bg-indigo-600 rounded" @click="saveName">
         Submit
     </button>
@@ -9,12 +13,16 @@
 <script setup>
 import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 const store = useStore()
+const router = useRouter()
 const name = computed(() => {
     return store.state.user.name
 })
 const newName = ref('')
 function saveName() {
     store.dispatch('saveName', newName.value)
+    newName.value = ''
+    router.push('/about')
 }
 </script>
